4570580d9d5ddc643102d17117a7f66a150d9b8b
Andrei Dragomir <adragomi@adobe.com>
2014-09-03 16:20:45 +0300

    remove partitioned mode

--- build-image.sh
+++ build-image.sh
@@ -9,17 +9,9 @@ prepare_dirs $location
 # detect partition type
 uuid=$(/sbin/blkid -o value -s UUID /dev/xvda1)
 
-echo "Making image !!!"
-if [ $mode == "partitioned" ];
-then
-  make_mbr_image $location/out/$name $size $uuid
-  # nouuid so that we don't throw errors
-  mount_partitioned_image $location/mnt
-else
-  make_raw_image $location/out/$name $size $uuid
-  # nouuid so that we don't throw errors
-  mount_raw_image $location/out/$name $location/mnt
-fi
+make_raw_image $location/out/$name $size $uuid
+# nouuid so that we don't throw errors
+mount_raw_image $location/out/$name $location/mnt
 
 # create a minimal system for chroot
 prepare_chroot $location/mnt
@@ -34,34 +26,10 @@ make_sysconfig_network_script  $location/mnt/etc/sysconfig/network-scripts/ifcfg
 # sshd for AWS
 make_sshd_config $location/mnt/etc/ssh/sshd_config
 
-if [ $mode == "partitioned"];
-then
-  # Install Grub. This might throw errors, just run it another time
-  cat > $location/mnt/boot/grub/device.map <<EOF
-  (hd0) $full_mapper
-EOF
-  install_grub $location/mnt
-  make_grub_conf $location/mnt "(hd0,0)" hvc0 $uuid
-  # fix device.map
-  cat > $location/mnt/boot/grub/device.map <<EOF
-  (hd0) /dev/xvda
-EOF
-  sync; sync; sync; sync
-else
-  make_grub_conf $location/mnt/boot/grub "(hd0)" hvc0 $uuid
-fi
+make_grub_conf $location/mnt/boot/grub "(hd0)" hvc0 $uuid
 
 unmount_image
 
-if [ $mode == "partitioned" ];
-then
-  finish_unmount_image_partitioned
-fi
-
-# Prepare bundling
 bundle_image $location/out/$name $name $location/out ""
-# upload
-upload_bundle $location/out/$name $s3_location
-RUBYLIB=/usr/lib/ruby/site_ruby/ ec2-upload-bundle --bucket $s3_location --manifest $location/out/$name.manifest.xml --access-key $access_key --secret-key $secret_key --retry --region $region
-# register
+upload_bundle $location/out/${name}.manifest.xml $s3_location
 register_image paravirtual $s3_location/$name.manifest.xml $name 
